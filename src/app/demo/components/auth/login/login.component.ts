7519fd821f9e59ef24a0a88dd2d67db0aa5c7464
sifez <herzisaif1@gmail.com>
2024-12-15 01:36:28 +0100

    proposition

--- src/app/demo/components/auth/login/login.component.ts
+++ src/app/demo/components/auth/login/login.component.ts
@@ -7,17 +7,24 @@ import { LayoutService } from 'src/app/layout/service/app.layout.service';
     styles: [`
         :host ::ng-deep .pi-eye,
         :host ::ng-deep .pi-eye-slash {
-            transform:scale(1.6);
+            transform: scale(1.6);
             margin-right: 1rem;
             color: var(--primary-color) !important;
+            cursor: pointer;
         }
     `]
 })
 export class LoginComponent {
 
-    valCheck: string[] = ['remember'];
-
-    password!: string;
+    valCheck: string[] = ['remember']; // Checkbox value for "Remember Me"
+    password!: string; // Password input value
+    cin!: string; // CIN input value
+    showPassword: boolean = false; // Toggle visibility of the password
 
     constructor(public layoutService: LayoutService) { }
+
+    // Method to toggle password visibility
+    togglePasswordVisibility(): void {
+        this.showPassword = !this.showPassword;
+    }
 }
